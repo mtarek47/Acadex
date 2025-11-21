bd070c91c3161fb1a58e54d3385309fc411d6f93
mtarek47 <mtarekrahman47@gmail.com>
2025-11-21 12:27:46 +0600

    removed admin lte things

--- src/main/java/com/example/acadex/config/SecurityConfig.java
+++ src/main/java/com/example/acadex/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:5173",
-                            "http://10.190.69.93:5173"
+                            "http://10.190.69.5173"
                     ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
